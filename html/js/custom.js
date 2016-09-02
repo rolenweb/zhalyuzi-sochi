562cf2fd33d81a481992e517c97f2761cc445a12
rolenweb <rolenweb@yandex.ru>
2016-09-02 15:11:51 +0300

    menu

--- html/js/custom.js
+++ html/js/custom.js
@@ -4,4 +4,9 @@ $(function() {
 		$(this).find('i').toggleClass('active');
     	home.find('div.top-header').find('div.top-contact-xs').toggle(1000);
     });
+
+    home.on('click', 'div.top-header li[name = "show-top-menu"]',function( event ) {  
+		$(this).find('i').toggleClass('active');
+    	home.find('div.top-menu').find('ul.top-menu-sm-xs').toggle(1000);
+    });
 });
