64fce417fa8863774cab578435406a927bfa79b9
rolenweb <rolenweb@yandex.ru>
2016-09-02 13:25:51 +0300

    fix

--- html/js/custom.js
+++ html/js/custom.js
@@ -1,6 +1,6 @@
 $(function() {
 	home = $('body');
-	home.on('click', 'div.top-header li[name = "show-contact"]',function( event ) {  
+	home.on('click', 'div.top-header i[name = "show-contact"]',function( event ) {  
 		$(this).find('i').toggleClass('active');
     	home.find('div.top-header').find('div.top-contact-xs').toggle(1000);
     });
