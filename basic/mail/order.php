b77a5f55eb0dd0a7aee1ca1426a35b2bc8303ecd
rolenweb <rolenweb@yandex.ru>
2016-09-11 15:13:35 +0300

    mail

--- basic/mail/order.php
+++ basic/mail/order.php
@@ -1,3 +1,50 @@
 <?php
-var_dump($order);
+echo Html::beginTag('table');
+	echo Html::beginTag('tr');
+		echo Html::beginTag('th');
+			echo 'Имя';
+		echo Html::endTag('th');
+		echo Html::endTag('td');
+			echo $order['name'];
+		echo Html::endTag('td');
+		echo Html::beginTag('th');
+			echo 'Телефон';
+		echo Html::endTag('th');
+		echo Html::endTag('td');
+			echo $order['phone'];
+		echo Html::endTag('td');
+
+		echo Html::beginTag('th');
+			echo 'Сумма';
+		echo Html::endTag('th');
+		echo Html::endTag('td');
+			echo $order['total'];
+		echo Html::endTag('td');
+
+		echo Html::beginTag('th');
+			echo 'Категория';
+		echo Html::endTag('th');
+		echo Html::endTag('td');
+			echo $order['category'];
+		echo Html::endTag('td');
+		echo Html::beginTag('th');
+			echo 'Подкатегория';
+		echo Html::endTag('th');
+		echo Html::endTag('td');
+			echo $order['sub-category'];
+		echo Html::endTag('td');
+		echo Html::beginTag('th');
+			echo 'Группа';
+		echo Html::endTag('th');
+		echo Html::endTag('td');
+			echo $order['group'];
+		echo Html::endTag('td');
+		echo Html::beginTag('th');
+			echo 'Материал';
+		echo Html::endTag('th');
+		echo Html::endTag('td');
+			echo $order['material'];
+		echo Html::endTag('td');
+	echo Html::endTag('tr');
+echo Html::endTag('table');
 ?>
