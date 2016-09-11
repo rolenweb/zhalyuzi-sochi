7b3f8fbf769e7cf5651b1cb637cbfd76b111bda8
rolenweb <rolenweb@yandex.ru>
2016-09-11 15:37:57 +0300

    fix

--- basic/mail/order.php
+++ basic/mail/order.php
@@ -17,47 +17,59 @@ echo Html::beginTag('table',['style' => 'border: 1px solid #000; text-align:left
 			echo $order['phone'];
 		echo Html::endTag('td');
 	echo Html::endTag('tr');
-	echo Html::beginTag('tr');
-		echo Html::beginTag('th');
-			echo 'Сумма';
-		echo Html::endTag('th');
-		echo Html::beginTag('td');
-			echo $order['total'].' руб.';
-		echo Html::endTag('td');
-	echo Html::endTag('tr');
-	echo Html::beginTag('tr');
-		echo Html::beginTag('th');
-			echo 'Категория';
-		echo Html::endTag('th');
-		echo Html::beginTag('td');
-			echo catName($order['category']);
-		echo Html::endTag('td');
-	echo Html::endTag('tr');
-	echo Html::beginTag('tr');
-		echo Html::beginTag('th');
-			echo 'Подкатегория';
-		echo Html::endTag('th');
-		echo Html::beginTag('td');
-			echo $order['sub-category'];
-		echo Html::endTag('td');
-	echo Html::endTag('tr');
+	if ($order['total'] != NULL) {
+		echo Html::beginTag('tr');
+			echo Html::beginTag('th');
+				echo 'Сумма';
+			echo Html::endTag('th');
+			echo Html::beginTag('td');
+				echo $order['total'].' руб.';
+			echo Html::endTag('td');
+		echo Html::endTag('tr');
+	}
+	if ($order['category'] != NULL) {
+		echo Html::beginTag('tr');
+			echo Html::beginTag('th');
+				echo 'Категория';
+			echo Html::endTag('th');
+			echo Html::beginTag('td');
+				echo catName($order['category']);
+			echo Html::endTag('td');
+		echo Html::endTag('tr');
+	}
+	if ($order['sub-category'] != NULL) {
+		echo Html::beginTag('tr');
+			echo Html::beginTag('th');
+				echo 'Подкатегория';
+			echo Html::endTag('th');
+			echo Html::beginTag('td');
+				echo $order['sub-category'];
+			echo Html::endTag('td');
+		echo Html::endTag('tr');
+	}
+	
 	if ($order['category'] == 'index' || $order['category'] == 'vertical-blinds' || $order['category'] == 'horizontal-blinds' || $order['category'] == 'plisse') {
-		echo Html::beginTag('tr');
-			echo Html::beginTag('th');
-				echo 'Группа';
-			echo Html::endTag('th');
-			echo Html::beginTag('td');
-				echo $order['group'];
-			echo Html::endTag('td');
-		echo Html::endTag('tr');
-		echo Html::beginTag('tr');
-			echo Html::beginTag('th');
-				echo 'Материал';
-			echo Html::endTag('th');
-			echo Html::beginTag('td');
-				echo $order['material'];
-			echo Html::endTag('td');
-		echo Html::endTag('tr');
+		if ($order['group'] != NULL) {
+			echo Html::beginTag('tr');
+				echo Html::beginTag('th');
+					echo 'Группа';
+				echo Html::endTag('th');
+				echo Html::beginTag('td');
+					echo $order['group'];
+				echo Html::endTag('td');
+			echo Html::endTag('tr');
+		}
+		if ($order['material'] != NULL) {
+			echo Html::beginTag('tr');
+				echo Html::beginTag('th');
+					echo 'Материал';
+				echo Html::endTag('th');
+				echo Html::beginTag('td');
+					echo $order['material'];
+				echo Html::endTag('td');
+			echo Html::endTag('tr');
+		}
+		
 	}
 	
 echo Html::endTag('table');
