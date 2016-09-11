bbd3d274e9ac58155ff904905f44c11f974c0e11
rolenweb <rolenweb@yandex.ru>
2016-09-11 15:33:07 +0300

    fix

--- basic/mail/order.php
+++ basic/mail/order.php
@@ -1,6 +1,6 @@
 <?php
 use yii\helpers\Html;
-echo Html::beginTag('table');
+echo Html::beginTag('table',['style' => 'border: 1px solid #000; text-align:left']);
 	echo Html::beginTag('tr');
 		echo Html::beginTag('th');
 			echo 'Имя';
@@ -22,7 +22,7 @@ echo Html::beginTag('table');
 			echo 'Сумма';
 		echo Html::endTag('th');
 		echo Html::beginTag('td');
-			echo $order['total'];
+			echo $order['total'].' руб.';
 		echo Html::endTag('td');
 	echo Html::endTag('tr');
 	echo Html::beginTag('tr');
@@ -30,7 +30,7 @@ echo Html::beginTag('table');
 			echo 'Категория';
 		echo Html::endTag('th');
 		echo Html::beginTag('td');
-			echo $order['category'];
+			echo catName($order['category']);
 		echo Html::endTag('td');
 	echo Html::endTag('tr');
 	echo Html::beginTag('tr');
@@ -41,21 +41,49 @@ echo Html::beginTag('table');
 			echo $order['sub-category'];
 		echo Html::endTag('td');
 	echo Html::endTag('tr');
-	echo Html::beginTag('tr');
-		echo Html::beginTag('th');
-			echo 'Группа';
-		echo Html::endTag('th');
-		echo Html::beginTag('td');
-			echo $order['group'];
-		echo Html::endTag('td');
-	echo Html::endTag('tr');
-	echo Html::beginTag('tr');
-		echo Html::beginTag('th');
-			echo 'Материал';
-		echo Html::endTag('th');
-		echo Html::beginTag('td');
-			echo $order['material'];
-		echo Html::endTag('td');
-	echo Html::endTag('tr');
+	if ($order['category'] == 'index' || $order['category'] == 'vertical-blinds' || $order['category'] == 'horizontal-blinds' || $order['category'] == 'plisse') {
+		echo Html::beginTag('tr');
+			echo Html::beginTag('th');
+				echo 'Группа';
+			echo Html::endTag('th');
+			echo Html::beginTag('td');
+				echo $order['group'];
+			echo Html::endTag('td');
+		echo Html::endTag('tr');
+		echo Html::beginTag('tr');
+			echo Html::beginTag('th');
+				echo 'Материал';
+			echo Html::endTag('th');
+			echo Html::beginTag('td');
+				echo $order['material'];
+			echo Html::endTag('td');
+		echo Html::endTag('tr');
+	}
+	
 echo Html::endTag('table');
+
+public function catName($cat)
+{
+	if ($cat == 'index') {
+		return 'Рулонные шторы';
+	}
+	if ($cat == 'vertical-blinds') {
+		return 'Вертикальные жалюзи';
+	}
+	if ($cat == 'horizontal-blinds') {
+		return 'Горизонтальные жалюзи';
+	}
+	if ($cat == 'plisse') {
+		return 'Плисе';
+	}
+	if ($cat == 'cornice') {
+		return 'Карнизы';
+	}
+	if ($cat == 'mosquito-nets') {
+		return 'Москитные сетки';
+	}
+	if ($cat == 'electro-blinds') {
+		return 'Электро жалюзи';
+	}
+}
 ?>
